79e483f6418237493e8af022292d8bd9eb58ff44
ranajoy007 <sahil.khanna1@wipro.com>
2015-12-01 11:08:33 +0530

    Create dynamic.cpp

---
+++ dynamic.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+
+#include<string>
+
+using namespace std;
+
+
+class Array
+{
+    
+   
+int size;
+   
+int *arr;
+  
+public:
+
+Array()
+{
+    arr=0;
+    size=0;
+}
+    
+Array(int ss)
+    
+{
+        
+size=ss;
+        
+arr=new int[size];
+        
+  
+  
+}
+    
+    
+
+void add(int n)
+    
+{
+        
+cout<<"enter array elements"<<endl;
+        
+int j=n;
+        
+for(int i=0;i<size;i++)
+        
+{
+         
+
+            
+arr[i]=j;
+            
+j++;
+        
+    
+}
+   
+}
+
+   
+
+void addNext(int a)
+    
+{
+   int i; 
+      
+int *ptr=new int[size+1];
+        
+for(i=0;i<size;i++)
+        
+{
+           
+ptr[i]=arr[i];
+                            
+}
+
+                 
+ptr[i]=a;
+                
+if(arr!=NULL)
+                        
+delete[] arr;
+                 
+arr=ptr;
+    
+}
+
+
+
+
+void disp()
+
+{
+
+for(int i=0;i<=size;i++)
+
+{
+cout<<arr[i]<<endl;
+}
+
+}
+
+Array(const Array & obj);
+
+~Array()
+{
+cout<<"destructor called";
+if(arr!=NULL)
+delete [] arr;
+}
+
+};
+
+Array::Array(const Array & obj)
+{
+size=obj.size;
+    arr=new int[size];
+    for(int i=0;i<size;i++)
+    arr[i]=obj.arr[i];
+}
+
+int main()
+
+{
+    
+Array aa(10);
+
+aa.add(60);
+
+aa.addNext(56);
+
+//aa.disp();
+Array pp(11);
+
+pp=aa;
+pp.disp();
+
+return 0;
+
+}
+
+
